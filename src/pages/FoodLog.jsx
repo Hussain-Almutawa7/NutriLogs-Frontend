e81ebb5b103a9dd71c105675ed54c6bccbe56216
Hussain-Almutawa7 <hussain.almutawa99@gmail.com>
2026-08-11 00:54:01 +0300

    Test entry values and change error handling position

--- src/pages/FoodLog.jsx
+++ src/pages/FoodLog.jsx
@@ -31,14 +31,35 @@ function FoodLog() {
         fetchEntryDetails();
     }, [selectedDate]);
 
-    if (isLoading) return <p>Loading Food Logs...</p>
-    if (error) return <p>Error Occured: {error}</p>
-    if (entries.length === 0) return <p>No Food Log entries.</p>
-
+    function handleChange(e) {
+        setSelectedDate(e.target.value);
+    }
 
     return (
         <main>
-            <h1>Here Will be the food Log page</h1>
+            <h1>Food Log</h1>
+            <input type="date" value={selectedDate} onChange={handleChange} />
+
+            {isLoading ? (
+                <p>Loading Food Logs...</p>
+            ) : error ? (
+                <p>Error Occurred: {error}</p>
+            ) : entries.length === 0 ? (
+                <p>No food log entries.</p>
+            ) : (
+                entries.map(entry => (
+                    <div key={entry._id}>
+                        <p>{entry.time}</p>
+                        <p>{entry.foodName}</p>
+                        <p>{entry.consumedAmount} {entry.consumedUnit}</p>
+                        <p>{entry.totalCalories} Kcal</p>
+                        <p>{entry.totalProtein === null ? "N/A" : entry.totalProtein + "g"} Protein</p>
+                        <p>{entry.totalCarbohydrates === null ? "N/A" : entry.totalCarbohydrates + "g"} Carbohydrates</p>
+                        <p>{entry.totalFat === null ? "N/A" : entry.totalFat + "g"} Fat</p>
+                    </div>
+                ))
+            )}
+
         </main>
     );
 }
