8828c60116cf3b090ac5b580b3d3f4d38276a773
Hussain-Almutawa7 <hussain.almutawa99@gmail.com>
2026-08-12 01:13:28 +0300

    Pass required information to the log food form to utilize it for edit

--- src/pages/EditFoodLogEntryPage.jsx
+++ src/pages/EditFoodLogEntryPage.jsx
@@ -4,13 +4,37 @@ import { useParams } from "react-router";
 import { showEntry } from "../services/foodLogService";
 
 function EditFoodLogEntryPage() {
-    const { entryId } = useParams();
 
-    
+    const { entryId } = useParams();
+    const navigate = useNavigate();
+
+    const [entry, setEntry] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState("");
+
+    useEffect(() => {
+        const fetchEntryDetails = async () => {
+            try {
+                const entryData = await showEntry(entryId);
+                setEntry(entryData);
+            } catch (e) {
+                setError(e.message);
+            } finally {
+                setIsLoading(false);
+            }
+        }
+
+        fetchEntryDetails();
+    }, [entryId]);
+
+    if (isLoading) return <p>Loading entry...</p>
+    if (error) return <p>Error: {error}</p>
+    if (!entry) return <p>No entry Found</p>
+
     return (
-        <div>
-            <h1>Edit page</h1>
-        </div>
+        <main>
+            <LogFoodForm entry={entry} entryId={entryId} mode={"edit"} />
+        </main>
     );
 }
 
