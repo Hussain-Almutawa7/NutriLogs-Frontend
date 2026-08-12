9cd7ea8a32c2013505f2299eee57a4730c788880
Hussain-Almutawa7 <hussain.almutawa99@gmail.com>
2026-08-13 01:54:22 +0300

    Add sign in and create account buttons on landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router";
+
 function Landing() {
     return (
         <div className="landing">
@@ -6,6 +8,16 @@ function Landing() {
                 Track your food, nutrition goals, and daily progress
                 in one simple place. Sign in or create an account to begin.
             </p>
+
+            <div className="landing-actions">
+                <Link to="/sign-up">
+                    <button>Create Account</button>
+                </Link>
+
+                <Link to="/sign-in">
+                    <button className="secondary-button">Sign In</button>
+                </Link>
+            </div>
         </div>
     )
 }
